e69b24fd3f8d378d55323825e1fc68eaea4baef7
Victor Lambret <lambret.victor@gmail.com>
2023-10-10 16:03:55 +0200

    Use collection.Counter to groupe tickets by day

--- TeamProgress.py
+++ TeamProgress.py
@@ -1,7 +1,10 @@
+import collections
+
+
 class TeamProgress:
 
-    def __init__(self, finished_ticket_dates):
-        pass
+    def __init__(self, finished_ticket_dates: list):
+        self.finished_tickets = collections.Counter(finished_ticket_dates)
 
-    def ticket_closed_at(self, param):
-        return 1
+    def ticket_closed_at(self, date):
+        return self.finished_tickets[date]
--- TeamProgressTest.py
+++ TeamProgressTest.py
@@ -12,6 +12,25 @@ class TeamProgressTest(unittest.TestCase):
         progress = TeamProgress(finished_ticket_dates)
         self.assertEqual(1, progress.ticket_closed_at("2010-01-01"))  # add assertion here
 
+    def test_progress_count_all_tickets_closed_on_a_day(self):
+        finished_ticket_dates = [
+            "2010-01-03",
+            "2010-01-02",
+            "2010-01-03",
+        ]
+
+        progress = TeamProgress(finished_ticket_dates)
+        self.assertEqual(1, progress.ticket_closed_at("2010-01-02"))  # add assertion here
+        self.assertEqual(2, progress.ticket_closed_at("2010-01-03"))  # add assertion here
+
+    def test_zero_is_returned_when_no_tickets_are_closed_that_day(self):
+        finished_ticket_dates = [
+            "2010-01-04",
+        ]
+
+        progress = TeamProgress(finished_ticket_dates)
+        self.assertEqual(0, progress.ticket_closed_at("2010-01-05"))  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()

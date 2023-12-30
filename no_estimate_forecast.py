9bc7e8a3a4c5581854b8969feafb3865372a4f6e
Victor Lambret <lambret.victor@gmail.com>
2023-12-30 20:01:52 +0100

    refactor

--- no_estimate_forecast.py
+++ no_estimate_forecast.py
@@ -1,36 +1,19 @@
 from cumulative_flow import CumulativeFlow
 
 
-class ForecastUsingLast:
-
-    def __init__(self, closed_task_that_day: float, task_velocity_per_day: float):
-        self.closed_task_that_day = closed_task_that_day
-        self.task_velocity_per_day = task_velocity_per_day
-
-    def on_next_days(self, number_of_days: int) -> float:
-        return self.closed_task_that_day + self.task_velocity_per_day * number_of_days
-
-
-class ForecastOnDay:
-
-    def __init__(self, flow, day_number: int):
-        self.cumulative_flow: CumulativeFlow = flow
-        self.day_number: int = day_number
-
-    def using_last_days(self, number_of_past_days) -> ForecastUsingLast:
-        closed_that_day = self.cumulative_flow.get_total_closed_task_on_day(self.day_number)
-        closed_before_interval = self.cumulative_flow.get_total_closed_task_on_day(self.day_number - number_of_past_days)
-        closed_during_interval = closed_that_day - closed_before_interval
-
-        task_velocity_per_day = closed_during_interval / number_of_past_days
-        return ForecastUsingLast(closed_that_day, task_velocity_per_day)
-
-
 class NoEstimateForecast:
     def __init__(self, cumulative_flow: CumulativeFlow, using_last_days: int, on_the_next_days: int):
         self.cumulative_flow = cumulative_flow
-        self.using_last_days = using_last_days
-        self.on_the_next_days = on_the_next_days
+        self.number_of_day_used_for_velocity = using_last_days
+        self.number_of_days_in_the_future = on_the_next_days
 
     def forecast_on_day(self, forecast_day) -> float:
-        return ForecastOnDay(self.cumulative_flow, forecast_day).using_last_days(self.using_last_days).on_next_days(self.on_the_next_days)
+        total_closed_tasks_that_day = self.cumulative_flow.get_total_closed_task_on_day(forecast_day)
+
+        first_day_of_velocity_interval = forecast_day - self.number_of_day_used_for_velocity
+        closed_before_interval = self.cumulative_flow.get_total_closed_task_on_day(first_day_of_velocity_interval)
+
+        closed_during_interval = total_closed_tasks_that_day - closed_before_interval
+        task_velocity_per_day = closed_during_interval / self.number_of_day_used_for_velocity
+
+        return total_closed_tasks_that_day + task_velocity_per_day * self.number_of_days_in_the_future

c7aa0cbffd8f6207d9ac9876dff08f3485bf8871
Victor Lambret <lambret.victor@gmail.com>
2023-12-30 23:20:26 +0100

    add signed MMRE

--- mmre.py
+++ mmre.py
@@ -1,8 +1,10 @@
 
 # MMRE: Mean Magnitude of Relative Error
+def compute_signed_mmre(actual: float, estimated: float) -> float:
+    return (estimated - actual) / actual
 def compute_mmre(actual: float, estimated: float) -> float:
     # MMRE: Absolute Value((Actual result - Estimated result) / Actual Result)
-    return abs(actual - estimated) / actual
+    return abs(compute_signed_mmre(actual, estimated))
 
 def compute_all_mmre(actuals: list[float], estimateds: list[float]) -> list[float]:
     return [compute_mmre(actuals[index], estimateds[index]) for index, _ in enumerate(actuals)]
--- test_mmre.py
+++ test_mmre.py
@@ -1,5 +1,5 @@
 import pytest
-from mmre import compute_all_mmre, compute_mmre
+from mmre import compute_all_mmre, compute_mmre, compute_signed_mmre
 
 
 class TestMRE:
@@ -13,12 +13,19 @@ class TestMRE:
     def test_mmre_formula(self, actual, estimated, expected_result):
         assert compute_mmre(actual, estimated) == expected_result
 
+    @pytest.mark.parametrize("actual, estimated, expected_result", [
+        (1.0, 1.0, 0.0),
+        (100.0, 100.0, 0.0),
+        (100.0, 200.0, 1.0),
+        (200.0, 100.0, -0.5),
+        (100.0, 400.0, 3.0),
+    ])
+    def test_signed_mmre_formula(self, actual, estimated, expected_result):
+        assert compute_signed_mmre(actual, estimated) == expected_result
+
     def test_perfect_forecast(self):
         actual_project_cumulative_flow = [1, 2, 2, 4, 5]
         forecast_project_cumulative_flow = [1, 2, 2, 2, 10]
 
         actual_all_mmre = compute_all_mmre(actual_project_cumulative_flow, forecast_project_cumulative_flow)
         assert actual_all_mmre == [0.0, 0.0, 0.0, 0.5, 1.0]
-
-
-

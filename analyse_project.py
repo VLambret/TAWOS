6c9b29c6032d54c42668ad5ef9fd41cb0b09df0c
Victor Lambret <lambret.victor@gmail.com>
2023-12-31 01:21:08 +0100

    Add labels and legends

--- analyse_project.py
+++ analyse_project.py
@@ -9,23 +9,24 @@ from cumulative_flow import CumulativeFlow
 from no_estimate_forecast import NoEstimateForecast
 
 
-def show_graph(filename: Path, real_progress: dict[date, float], estimates: dict[date, float]):
-    progress_dates = list(real_progress.keys())
-    progress_values = list(real_progress.values())
-
-    estimates_dates = list(estimates.keys())
-    estimates_values = list(estimates.values())
-
+def show_graph(filename: Path, real_progress: dict[date, float], estimates: dict[str, dict[date, float]]):
     figure = matplotlib.figure.Figure(figsize=(8, 6))
 
     figure_axis = figure.add_subplot()
     figure.suptitle(f'{filename} - Completed issues over time')
-
     figure_axis.set_xlabel('Date')
     figure_axis.set_ylabel('# of Completed issues')
 
-    figure_axis.plot(progress_dates, progress_values)
-    figure_axis.plot(estimates_dates, estimates_values)
+    progress_dates = list(real_progress.keys())
+    progress_values = list(real_progress.values())
+    figure_axis.plot(progress_dates, progress_values, label='Actual')
+
+    estimates_dates = list(estimates.keys())
+    estimates_values = list(estimates.values())
+    figure_axis.plot(estimates_dates, estimates_values, label='Estimates')
+
+    figure_axis.legend()
+
     figure.savefig(filename, dpi=300)
 
 
@@ -42,12 +43,16 @@ def main():
 
     cumulated_completed_task_per_day = project_activity.total_closed_task_per_day
 
-    forecaster = NoEstimateForecast(project_activity, 100, 100)
+    all_estimates = {}
 
-    estimates = forecaster.forecast_all_days()
-    estimates_with_dates = {}
-    for index, day in enumerate(cumulated_completed_task_per_day.keys()):
-        estimates_with_dates[day] = estimates[index]
+    for n in [30, 90, 180, 360]:
+        forecaster = NoEstimateForecast(project_activity, n, n)
+        estimates = forecaster.forecast_all_days()
+        estimates_with_dates = {}
+        for index, day in enumerate(cumulated_completed_task_per_day.keys()):
+            estimates_with_dates[day] = estimates[index]
+
+        all_estimates[f'{n}'] = estimates_with_dates
 
 
     project_graph_file = Path(sys.argv[1]).parent / "graph_actual_work_and_estimates"

db3945b882a4c48260c6cf2fc1fa22b66702a1e3
Victor Lambret <lambret.victor@gmail.com>
2023-12-31 18:47:10 +0100

    DatedValues: eq

--- dated_values.py
+++ dated_values.py
@@ -1,3 +1,16 @@
 from datetime import date
 
 DatedValuesType = dict[date, [float | int]]
+
+
+class DatedValues:
+
+    def __init__(self, values: DatedValuesType):
+        self.values = values
+
+    def __eq__(self, other):
+        if not isinstance(other, DatedValues):
+            return False
+
+        return self.values == other.values
+
--- test_dated_values.py
+++ test_dated_values.py
@@ -1,11 +1,23 @@
 from datetime import date
 
+from dated_values import DatedValues
+
 
 class TestDatedValues:
-    def test_can_be_compared(self):
-        task_end_dates = {
-            date(2023, 1, 3): 42,
-            date(2023, 1, 3): 43
-        }
+    data1 = {
+        date(2023, 1, 3): 42,
+        date(2023, 1, 3): 43
+    }
 
-        values1= None
+    data2 = {
+        date(2023, 1, 3): 52,
+        date(2023, 1, 3): 53
+    }
+
+    values1 = DatedValues(data1)
+    values2 = DatedValues(data1)
+    values3 = DatedValues(data2)
+
+    def test_can_be_compared(self):
+        assert self.values1 == self.values2
+        assert self.values1 != self.values3

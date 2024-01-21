9fb8e63542a8f14a1a1d4b0fbf1b3cdf30ed4654
Victor Lambret <lambret.victor@gmail.com>
2024-01-21 13:10:45 +0100

    GREEN

--- src/time_series/cumulative_time_series.py
+++ src/time_series/cumulative_time_series.py
@@ -18,7 +18,7 @@ class CumulativeTimeSeries:
 
     def __init__(self, values: DatedValuesType):
         self.values: list[DatedValue] = [DatedValue(d, v) for d, v in values.items()]
-        self.values2 = OrderedDict()
+        self.values2: OrderedDict[date: float] = OrderedDict()
 
     def __getitem__(self, day_number):
         return self.values[day_number - 1]

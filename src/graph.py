ed4c2c76575489e467c75f9cecb4c91cdc98737b
Victor Lambret <lambret.victor@gmail.com>
2024-02-06 19:40:45 +0100

    Fix compilation

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,5 @@
 import re
+from typing import Dict
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
@@ -6,7 +7,27 @@ from matplotlib.figure import Figure
 from time_series.cumulative_time_series import CumulativeTimeSeries
 
 
-def save_as_graph(project, title: str, x_label, y_label, all_data_to_plot: dict[str, CumulativeTimeSeries]):
+def save_as_graph(project, title: str, x_label, y_label, all_data_to_plot: Dict[str, Dict[int, float]]):
+    figure = Figure(figsize=(8, 6))
+
+    filename = project.folder / (_sanitize(title) + ".png")
+
+    figure_axis = figure.add_subplot()
+    figure.suptitle(f"{project.name} - {title}")
+    figure_axis.set_xlabel(x_label)
+    figure_axis.set_ylabel(y_label)
+
+    for label, data in all_data_to_plot.items():
+        # TODO: make all data to plot a dict and not a CumulativeTimeSeries
+        estimates_dates = data.keys()
+        estimates_values = data.values()
+        figure_axis.plot(estimates_dates, estimates_values, label=label)
+
+    figure_axis.legend()
+
+    figure.savefig(filename, dpi=300)
+
+def save_as_graph_old(project, title: str, x_label, y_label, all_data_to_plot: dict[str, CumulativeTimeSeries]):
     figure = Figure(figsize=(8, 6))
 
     filename = project.folder / (_sanitize(title) + ".png")
--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ import json
 import sys
 from datetime import date, datetime
 from pathlib import Path
+from typing import Dict
 
 from evaluation_tools.metrics import compute_mmre, compute_signed_mmre, compute_mmre_quality, compute_all_risk_being_late
-from graph import save_as_graph
+from graph import save_as_graph_old, save_as_graph
 from time_series.normalized_time_series import NormalizedTimeSeries
 from time_series.cumulative_time_series import CumulativeTimeSeries
 from forecasts.no_estimate_forecast import NoEstimateForecast
@@ -27,13 +28,17 @@ def load_dates_from(project_csv: Path) -> list[date]:
         return [datetime.strptime(d.strip(), "%Y-%m-%d").date() for d in f.readlines()]
 
 
-def save_as_json(project: Project, mmre_quality: dict[str, CumulativeTimeSeries], file_name) -> None:
+def save_as_json_old(project: Project, mmre_quality: dict[str, CumulativeTimeSeries], file_name) -> None:
     result: dict = {project.name: {
         v.date :v.value
         for v in mmre_quality['MMRE quality'].values
     }}
     with open(project.folder / file_name, 'w') as json_file:
         json.dump(result, json_file, indent=4, default=str)
+
+def save_as_json(project: Project, data: Dict[str, Dict[int, float]], file_name) -> None:
+    with open(project.folder / file_name, 'w') as json_file:
+        json.dump(data, json_file, indent=4, default=str)
 
 
 def main() -> None:
@@ -52,11 +57,11 @@ def main() -> None:
         "Unfiltered": unfiltered_real_total_completed_tasks_per_day,
         "Filtered": filtered_real_total_completed_tasks_per_day
     }
-    save_as_graph(project,
+    save_as_graph_old(project,
                   "real activity ",
                   "Date",
                   'total completed issues',
-                  unfiltered_reality)
+                      unfiltered_reality)
 
     reality = {
         "Reality": filtered_real_total_completed_tasks_per_day
@@ -72,51 +77,51 @@ def main() -> None:
     for n in [180, 360]:
         all_estimates_to_plot[f'{n} days'] = all_total_completed_tasks_per_day_estimates[n]
 
-    save_as_graph(project,
+    save_as_graph_old(project,
                   "cumulated completed task forecasts",
                   "Date",
                   'total completed issues',
-                  all_estimates_to_plot)
+                      all_estimates_to_plot)
 
     # Signed MMRE
     signed_mmre_to_plot = compute_signed_mmre(real_total_completed_tasks_per_day,
                                               all_estimates_to_plot)
-    save_as_graph(project,
+    save_as_graph_old(project,
                   "cumulated completed task forecasts signed MMRE",
                   "Date",
                   'Signed MMRE',
-                  signed_mmre_to_plot)
+                      signed_mmre_to_plot)
 
     # MMRE
     mmre_to_plot = compute_mmre(real_total_completed_tasks_per_day,
                                 all_estimates_to_plot)
-    save_as_graph(project,
+    save_as_graph_old(project,
                   "cumulated completed task forecasts MMRE",
                   "Date",
                   'MMRE',
-                  mmre_to_plot)
+                      mmre_to_plot)
 
     # MMRE Quality
     mmre_quality = compute_mmre_quality(all_total_completed_tasks_per_day_estimates,
                                         real_total_completed_tasks_per_day)
 
-    save_as_graph(project,
+    save_as_graph_old(project,
                   "Average MMRE for each period",
                   "Date",
                   'Average MMRE',
-                  mmre_quality)
+                      mmre_quality)
 
-    save_as_json(project, mmre_quality, 'mmre_quality.json')
+    save_as_json_old(project, mmre_quality, 'mmre_quality.json')
 
     # Risk being late
     risk_being_late = compute_all_risk_being_late(all_total_completed_tasks_per_day_estimates,
                                                   real_total_completed_tasks_per_day)
 
     save_as_graph(project,
-                  "Average MMRE for each period",
+                  "Risk being late",
                   "Date",
                   'Average MMRE',
-                  risk_being_late)
+                      risk_being_late)
 
     save_as_json(project, risk_being_late, 'risk_being_late.json')
 
